e3e9787c2e6fa3ae4c887dd3c45a6ba627066632
Avinash Kumar <avinashbest3@gmail.com>
2020-10-02 12:31:49 +0530

    Added Day 21

---
+++ Day 21 30 Sept/HomeWork/Assignment2.c
@@ -0,0 +1,46 @@
+//  WAP to create a queue using array implementation and display only even numbers.
+#include <stdio.h>
+
+int queue[10];
+int rear, front;
+
+void insert();
+void show();
+
+int main(void)
+{
+    front = -1;
+    rear = -1;
+    insert();
+    show();
+    return 0;
+}
+
+void insert()
+{
+    if (rear == 9)
+    {
+        printf("\nQueue is Full!\n");
+    }
+    else
+    {
+        front = 0;
+        rear = 0;
+        for (; rear < 10; rear++)
+        {
+            printf("Enter Element in Queue: ");
+            scanf("%d", &queue[rear]);
+        }
+    }
+}
+
+void show()
+{
+    for (; front < rear; front++)
+    {
+        if (queue[front] % 2 == 0)
+        {
+            printf("%d\t", queue[front]);
+        }
+    }
+}
